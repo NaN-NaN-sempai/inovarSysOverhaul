77e25bd99b0ef91241906f64ec14b4f72fa32517
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2022-11-14 23:32:43 -0300

    minor fixes and changes

--- server.js
+++ server.js
@@ -143,10 +143,16 @@ setInterval(() => {
                             width: 60vw;
                             height: 80vh;
                             opacity: .5;
+                            filter: blur(2px);
                             pointer-events: auto;
+                        }
+                        #driveDisplayContainer.show.offFocus {
+                            opacity: 1;
+                            filter: blur(0px);
                         }
                         #driveDisplayContainer.show:hover {
                             opacity: 1;
+                            filter: blur(0px);
                         }
                         .driveIcon {
                             height: 30px;
@@ -171,141 +177,147 @@ setInterval(() => {
                     </style>
                     <div class="insertHtmlMainContainer">
                         <div id="contentDisplayContainer">
-                            asdasdasdasd
+                            Carregando...
                         </div>
 
-                        <div id="driveDisplayContainer">
-                            <iframe class="contentDisplayIframe" src="https://drive.google.com/drive/u/0/my-drive"></iframe>
+                        <div id="driveDisplayContainer" style="${window.removeDriveContainerBlur?"":"filter: blur(0px)"}">
+                            <iframe class="contentDisplayIframe" src="https://drive.google.com/drive/u/0/starred"></iframe>
                         </div>
                         
                         <div class="sideButtonsContainer">
+                            <p class="clickableButton" onclick="apex.submit()">Recarregar lista</p>
                             <p class="clickableButton" onclick="window.openContentInDisplay(this.innerHTML)">Dados do pedido</p>
                             <p class="clickableButton" onclick="document.querySelector('#driveDisplayContainer').classList.toggle('show'); this.style.opacity=''">
                                 <img class="driveIcon" src="https://cdn-icons-png.flaticon.com/512/5968/5968523.png">
                             </p>
-                            <p class="clickableButton" onclick="apex.submit()">Recarregar lista</p>
                             <p class="clickableButton" onclick="window.openContentInDisplay(this.innerHTML)">⚙</p>
                         </div>
                     </div>
                 `;
                 //html
+                document.addEventListener('mouseleave', evt => {
+                    document.querySelector('#driveDisplayContainer').classList.add("offFocus");
+                });
+                document.addEventListener('mouseenter', evt => {
+                    document.querySelector('#driveDisplayContainer').classList.remove("offFocus");
+                });
                 document.body.append(div);
                 }
             } else {
                 if(placeHtml){
-                placeHtml = false;
+                    placeHtml = false;
 
-                var pType = document.querySelector(".t-Dialog")?.querySelector("input").id.slice(0,2).toUpperCase();
+                    var pType = document.querySelector(".t-Dialog")?.querySelector("input").id.slice(0,2).toUpperCase();
 
-                var div2 = document.createElement("div");
-                div2.style.display = "flex";
-                div2.innerHTML = `<style>
-                .clickableButton {
-                  background: white;
-                  color: black;
-                  border-radius: 10px;
-                  text-align: center;
-                  border: 1px black solid;
-                  cursor: pointer;
-                  user-select: none;
-                  margin-right: 5px;
-                  margin-bottom: 5px;
-                  padding-left: 4px;
-                  padding-right: 4px;
-                }
-                .stateButton {
-                  display: inline-flex;
-                  align-items: center;
-                  font-size: 13px;
-                  line-height: 13px;
-                  height: 50px;
-                }
-                .stateSelected {
-                  background: cyan;
-                }
-                .removeName {
-                  background: red;
-                  color: white;
-                  margin-left: 10px;
-                }
-                </script>`;
-
-                if(document.getElementById(pType+"_SITUACAO_DESIGN")){
-                    window.insertValueInInput = (element) => {
-                        document.querySelector(".stateSelected")?.classList.remove("stateSelected");
-                        element.classList.add("stateSelected");
-                        document.querySelector("#"+pType+"_SITUACAO_DESIGN").value = element.innerHTML;
+                    var div2 = document.createElement("div");
+                    div2.style.display = "flex";
+                    div2.innerHTML = `<style>
+                    .clickableButton {
+                    background: white;
+                    color: black;
+                    border-radius: 10px;
+                    text-align: center;
+                    border: 1px black solid;
+                    cursor: pointer;
+                    user-select: none;
+                    margin-right: 5px;
+                    margin-bottom: 5px;
+                    padding-left: 4px;
+                    padding-right: 4px;
                     }
-                    document.getElementById(pType+"_SITUACAO_DESIGN").style.display = "none";
-                    Array.from(document.getElementById(pType+"_SITUACAO_DESIGN").children).forEach((e, i) => {
-                        if(i){
-                            div2.innerHTML += `<span class='clickableButton stateButton${e.value == document.querySelector("#"+pType+"_SITUACAO_DESIGN").value? " stateSelected": ""}' onclick='window.insertValueInInput(this)'>${e.value}</span>`;
+                    .stateButton {
+                    display: inline-flex;
+                    align-items: center;
+                    font-size: 13px;
+                    line-height: 13px;
+                    height: 50px;
+                    }
+                    .stateSelected {
+                    background: cyan;
+                    }
+                    .removeName {
+                    background: red;
+                    color: white;
+                    margin-left: 10px;
+                    }
+                    </script>`;
+
+                    if(document.getElementById(pType+"_SITUACAO_DESIGN")){
+                        window.insertValueInInput = (element) => {
+                            document.querySelector(".stateSelected")?.classList.remove("stateSelected");
+                            element.classList.add("stateSelected");
+                            document.querySelector("#"+pType+"_SITUACAO_DESIGN").value = element.innerHTML;
                         }
-                    });
-                    document.getElementById(pType+"_SITUACAO_DESIGN").parentNode.append(document.createElement("br"));
-                    document.getElementById(pType+"_SITUACAO_DESIGN").parentNode.append(div2);
-                    document.getElementById(pType+"_SITUACAO_DESIGN_LABEL").innerHTML = "";
+                        document.getElementById(pType+"_SITUACAO_DESIGN").style.display = "none";
+                        Array.from(document.getElementById(pType+"_SITUACAO_DESIGN").children).forEach((e, i) => {
+                            if(i){
+                                div2.innerHTML += `<span class='clickableButton stateButton${e.value == document.querySelector("#"+pType+"_SITUACAO_DESIGN").value? " stateSelected": ""}' onclick='window.insertValueInInput(this)'>${e.value}</span>`;
+                            }
+                        });
+                        document.getElementById(pType+"_SITUACAO_DESIGN").parentNode.append(document.createElement("br"));
+                        document.getElementById(pType+"_SITUACAO_DESIGN").parentNode.append(div2);
+                        document.getElementById(pType+"_SITUACAO_DESIGN_LABEL").innerHTML = "";
 
 
 
-                    var divRow = document.createElement("div");
-                        divRow.className = "row";
-                    var divHoldClass = document.createElement("div");
-                        divHoldClass.className = "t-Form-fieldContainer t-Form-fieldContainer--floatingLabel  apex-item-wrapper apex-item-wrapper--display-only";
-                        divHoldClass.id = "insertCustomUserScriptElements";
-                    divRow.append(divHoldClass);
+                        var divRow = document.createElement("div");
+                            divRow.className = "row";
+                        var divHoldClass = document.createElement("div");
+                            divHoldClass.className = "t-Form-fieldContainer t-Form-fieldContainer--floatingLabel  apex-item-wrapper apex-item-wrapper--display-only";
+                            divHoldClass.id = "insertCustomUserScriptElements";
+                        divRow.append(divHoldClass);
 
-                    document.getElementsByClassName("container")[1].append(divRow);
-                    var insertEls = document.getElementById("insertCustomUserScriptElements");
+                        document.getElementsByClassName("container")[1].append(divRow);
+                        var insertEls = document.getElementById("insertCustomUserScriptElements");
 
 
-                    var ipt = document.createElement("input");
-                        ipt.id = "specialStatusInput";
-                        ipt.placeholder = "Status Especial";
+                        var ipt = document.createElement("input");
+                            ipt.id = "specialStatusInput";
+                            ipt.placeholder = "Status Especial";
 
-                    var specialStatusButtonFunction = () => {
-                        var iptvalue = document.getElementById("specialStatusInput").value;
+                        var specialStatusButtonFunction = () => {
+                            var iptvalue = document.getElementById("specialStatusInput").value;
 
-                        var customOpt = document.getElementById("specialStatusValue");
-                        if(!customOpt){
-                            var createdCustomOpt = document.createElement("option");
-                            createdCustomOpt.innerHTML = "Status Customizado";
-                            createdCustomOpt.id = "specialStatusValue";
+                            var customOpt = document.getElementById("specialStatusValue");
+                            if(!customOpt){
+                                var createdCustomOpt = document.createElement("option");
+                                createdCustomOpt.innerHTML = "Status Customizado";
+                                createdCustomOpt.id = "specialStatusValue";
 
-                            document.getElementById(pType+"_SITUACAO_DESIGN").append(createdCustomOpt);
+                                document.getElementById(pType+"_SITUACAO_DESIGN").append(createdCustomOpt);
 
-                            customOpt = document.getElementById("specialStatusValue");
+                                customOpt = document.getElementById("specialStatusValue");
+                            }
+
+                            customOpt.value = iptvalue;
+                            document.getElementById(pType+"_SITUACAO_DESIGN").value = customOpt.value;
                         }
 
-                        customOpt.value = iptvalue;
-                        document.getElementById(pType+"_SITUACAO_DESIGN").value = customOpt.value;
+                        var btn = document.createElement("p");
+                            btn.className = "clickableButton";
+                            btn.innerHTML = "Enviar";
+                            btn.addEventListener("click", specialStatusButtonFunction);
+
+                        insertEls.append(ipt);
+                        insertEls.append(btn);
+
+                        var removeNameButtonFunction = () => {
+                            document.getElementById(pType+"_PROFISSIONAL"+(pType[1]=="4"?"2":"")).value = "";
+                            document.querySelector(pType[1]=="4"?"#B15419308413409793391":"#B13467958673203439742").click();
+                        }
+
+                        var removeNameButton = document.createElement("p");
+                            removeNameButton.className = "clickableButton removeName";
+                            removeNameButton.innerHTML = "Remover Nome";
+                            removeNameButton.addEventListener("click", removeNameButtonFunction);
+
+                        insertEls.append(removeNameButton);
+
                     }
-
-                    var btn = document.createElement("p");
-                        btn.className = "clickableButton";
-                        btn.innerHTML = "Enviar";
-                        btn.addEventListener("click", specialStatusButtonFunction);
-
-                    insertEls.append(ipt);
-                    insertEls.append(btn);
-
-                    var removeNameButtonFunction = () => {
-                        document.getElementById(pType+"_PROFISSIONAL"+(pType[1]=="4"?"2":"")).value = "";
-                        document.querySelector(pType[1]=="4"?"#B15419308413409793391":"#B13467958673203439742").click();
-                    }
-
-                    var removeNameButton = document.createElement("p");
-                        removeNameButton.className = "clickableButton removeName";
-                        removeNameButton.innerHTML = "Remover Nome";
-                        removeNameButton.addEventListener("click", removeNameButtonFunction);
-
-                    insertEls.append(removeNameButton);
-
-                }
                 }
 
-                }
             }
+        }
     } catch { /* ignore errors */ }
 }, 500);
 
@@ -415,6 +427,17 @@ window.setReplaceWhatsapp2 = (element) => {
     element.innerHTML = window.configReplaceWhatsapp2? "✔": "✖";
 }
 
+localStorage.removeDriveContainerBlur = localStorage.removeDriveContainerBlur == undefined? false: localStorage.removeDriveContainerBlur;
+window.removeDriveContainerBlur = JSON.parse(localStorage.removeDriveContainerBlur);
+window.setDriveContainerBlur = (element) => {
+    localStorage.removeDriveContainerBlur = !window.removeDriveContainerBlur;
+    window.removeDriveContainerBlur = JSON.parse(localStorage.removeDriveContainerBlur);
+
+    element.innerHTML = window.removeDriveContainerBlur? "✔": "✖";
+
+    document.querySelector('#driveDisplayContainer').style.filter = window.removeDriveContainerBlur? "": "blur(0px)";
+}
+
 window.openContentInDisplayLastChoice = undefined;
 window.openContentInDisplay = (content, forceShow) => {
     var display = document.querySelector("#contentDisplayContainer");
@@ -427,14 +450,23 @@ window.openContentInDisplay = (content, forceShow) => {
             showContent = window.orderDataHolder == undefined? "Nem um pedido selecionado.<br>Pressione o botão direito no link de contato de algum pedido para ver os dados do pedido.": window.orderDataHolder;
 
         } else {
-            showContent = `<h3>Configurações</h3><hr>
+            showContent = /* html */ `
+            <h3>Configurações</h3><hr>
+
             <h4>Modo escuro</h4>
             <p class="clickableButton" onclick="window.setDarkMode(this)">${window.configDarkModeActive? "✔": "✖"}</p><hr>
+
             <h4>Abrir Whatsapp direto no aplicativo</h4>
             <p class="clickableButton" onclick="window.setReplaceWhatsapp(this)">${window.configReplaceWhatsapp? "✔": "✖"}</p><hr>
+
             <h4>Abrir Whatsapp direto navegador <br>
             (sem passar pela pagina de pergunta)</h4>
-            <p class="clickableButton" onclick="window.setReplaceWhatsapp2(this)">${window.configReplaceWhatsapp2? "✔": "✖"}</p>`;
+            <p class="clickableButton" onclick="window.setReplaceWhatsapp2(this)">${window.configReplaceWhatsapp2? "✔": "✖"}</p><hr>
+
+            <h4>Ativar Blur da aba do Google Drive<br>
+            (o Blur é apenas de visual, ativado pode causar lentidão)</h4>
+            <p class="clickableButton" onclick="window.setDriveContainerBlur(this)">${window.removeDriveContainerBlur? "✔": "✖"}</p>`;
+            //html
         }
 
         return title + showContent;
